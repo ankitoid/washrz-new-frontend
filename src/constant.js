e8a3af2d9120588863b2fbfc23ac6d9cf91cc47e
AyushSinghGXI <ayush.singh@gxinewtworks.com>
2025-12-08 18:57:50 +0530

    changes in the constant for port

--- src/constant.js
+++ src/constant.js
@@ -1,8 +1,8 @@
 const baseUrl = `https://live-server-101289.wati.io`;
 const washrzserver = false
-  ? `http://localhost:5000`
+  ? `http://localhost:5001`
   : // : `https://washrzbackend.vercel.app`;
-    `http://3.108.221.246:5000`;
+    `http://3.108.221.246:5001`;
     // `https://backend.magha1.com`;
 const Authkey ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6ImF5dXNoc2luZ2g4NDIwMThAZ21haWwuY29tIiwibmFtZWlkIjoiYXl1c2hzaW5naDg0MjAxOEBnbWFpbC5jb20iLCJlbWFpbCI6ImF5dXNoc2luZ2g4NDIwMThAZ21haWwuY29tIiwiYXV0aF90aW1lIjoiMTIvMDgvMjAyNSAwNzoyMzo1MyIsInRlbmFudF9pZCI6IjEwMTI4OSIsImRiX25hbWUiOiJtdC1wcm9kLVRlbmFudHMiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOlsiVEVNUExBVEVfTUFOQUdFUiIsIkRFVkVMT1BFUiIsIkFVVE9NQVRJT05fTUFOQUdFUiJdLCJleHAiOjI1MzQwMjMwMDgwMCwiaXNzIjoiQ2xhcmVfQUkiLCJhdWQiOiJDbGFyZV9BSSJ9.NpVe1fi-RXRuNgCAGzFQLZT6dE7Y-rvlx1SYxLKZ_m4";
 export default { baseUrl, Authkey, washrzserver };''
